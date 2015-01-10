533eb1a3f48d77eb7677281289bdbce3dc074924
lshizue <lshizue@gmail.com>
2015-01-10 16:48:52 -0200

    improved comments

--- load.R
+++ load.R
@@ -3,7 +3,7 @@ loadData <- function(){
   fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file(fileurl, destfile="rawData.zip",  method="curl")
   
-  #unzip
+  #unzipping
   unzip("rawData.zip")
   
   # loading data
@@ -11,10 +11,12 @@ loadData <- function(){
   tmp <- read.csv.sql('household_power_consumption.txt',"select * from file where Date in ('1/2/2007','2/2/2007')", sep = ';', header = T)
   
   #formatting date
-  tmp$Date <- as.Date(tmp$Date,"%d/%m/%Y")
+  tmp$Date <- as.Date(tmp$Date,"%d/%m/%Y") 
+  
+  #formatting date and time 
   tmp$Date <- strptime(paste(tmp$Date, tmp$Time),  "%Y-%m-%d %H:%M:%S")
 
-  # making data available
+  # making data available globally
   dados <<- tmp
   
 }
